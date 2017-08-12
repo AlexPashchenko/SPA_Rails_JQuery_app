eb75adb8394b9553331ad81d7962fc1c73359b44
AlexPashchenko <sashka.gag@gmail.com>
2017-08-12 11:14:40 +0300

    Add pagination

--- javascript.js
+++ javascript.js
@@ -1,13 +1,5 @@
   $(document).ready(function(){
 
-    $('#table').paging({
-
-      limit: 5,
-      rowDisplayStyle: 'block',
-      activePage: 0,
-      rows: []
-    });
-
     var interestsArr = ['programming', 'travels', 'music', 'painting', 'dancing', 'reading', 'driving', 'fitness',
     'cookery', 'drinking']
     var user;
@@ -54,6 +46,7 @@
           $('#frm')[0].reset();
           id++;
           localStorage.setItem("id", id);
+          $('#table').pagFun();
           return false;
 
       }
@@ -117,6 +110,7 @@
         var tab = $('#table').html();
         localStorage.setItem('table', tab);
         $('#frm')[0].reset();
+        tab.pagFun();
         });
 
         // edit function
@@ -132,6 +126,35 @@
         var tab = $('#table').html();
         localStorage.setItem('table', tab);
           $('#frm')[0].reset();
-    });
+          $('#table').pagFun();
 
-  });
+    });
+    $(function pagFun() {
+      var totalRows = $('#table').find('tbody tr:has(td)').length;
+      var recordPerPage = 3;
+      var totalPages = Math.ceil(totalRows / recordPerPage);
+      var $pages = $('<div id="pages" ></div>');
+      for (i = 0; i < totalPages; i++) {
+          $('<button type="button" class="pagination">&nbsp;' + (i + 1) + '</button>').appendTo($pages);
+      }
+       $pages.appendTo('#pagin');
+       $('.pagination').hover(
+         function() { $(this).addClass('focus'); },
+         function() { $(this).removeClass('focus'); }
+       );
+       $('table').find('tbody tr:has(td)').hide();
+       var tr = $('table tbody tr:has(td)');
+       for (var i = 0; i <= recordPerPage - 1; i++) {
+         $(tr[i]).show();
+       }
+       $('button').click(function(event) {
+         $('#table').find('tbody tr:has(td)').hide();
+         var nBegin = ($(this).text() - 1) * recordPerPage;
+         var nEnd = $(this).text() * recordPerPage - 1;
+         for (var i = nBegin; i <= nEnd; i++)
+         {
+           $(tr[i]).show();
+         }
+      });
+    });
+});
