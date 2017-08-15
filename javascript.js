54cfb3c97f4aaa29fc9cd0b724d077ccf6126c71
AlexPashchenko <sashka.gag@gmail.com>
2017-08-15 18:15:36 +0300

    Code refactoring

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@
     var user;
     var table = document.getElementById('table');
     var tableUsers = new Array();
+    sortedTab = [];
     tableUsers = JSON.parse(localStorage.getItem("users"));
     if (tableUsers === null){
       tableUsers =[];
@@ -14,9 +15,8 @@
       id = 1;
     }
 
-
     //get data from array into table rows
-    $(function() {
+    $(function GetTable() {
       $.each(tableUsers, function (index, data) {
         var eachrow = "<tbody><tr>"
                  + "<td>" + data.Id + "</td>"
@@ -31,84 +31,84 @@
       });
     });
 
+    //set user object
+    function setUser() {
+        user ={
+          Id : id,
+          FirstName : $("#firstname").val(),
+          LastName : $('#lastname').val(),
+          Age : $("#age").val(),
+          Gender : $(':radio[name=gender]:checked').val(),
+          Interests : $(':checkbox[name=inter]:checked').map(function() {
+            return $(this).val();
+          }).get(),
+          Country :  $("#country").val()
+        };
+    }
 
-    //Pagination function
 
-    $(function Pag() {
-      var totalRows = $('#table').find('tbody tr:has(td)').length;
-      var recordPerPage = 10;
-      var totalPages = Math.ceil(totalRows / recordPerPage);
-      var $pages = $('<div id="pages" class="pagin" ></div>');
-      for (i = 0; i < totalPages; i++) {
-          $('<button type="button" class="pagination">&nbsp;' + (i + 1) + '</button>').appendTo($pages);
+    //validation of form data
+    function valodateForm() {
+      if($("#firstname").val() == '' | $('#lastname').val() == '' | $("#age").val() == '' | $("#age").val() >111){
+        alert(":Incorrect Name or Age");
+        GetTable();
+        return false;
       }
-       $pages.appendTo('#pagin');
-       $('.pagination').hover(
-         function() { $(this).addClass('focus'); },
-         function() { $(this).removeClass('focus'); }
-       );
-       $('table').find('tbody tr:has(td)').hide();
-       var tr = $('table tbody tr:has(td)');
-       for (var i = 0; i <= recordPerPage - 1; i++) {
-         $(tr[i]).show();
-       }
-       $('button').on('click', function(event) {
-         $('#table').find('tbody tr:has(td)').hide();
-         var nBegin = ($(this).text() - 1) * recordPerPage;
-         var nEnd = $(this).text() * recordPerPage - 1;
-         for (var i = nBegin; i <= nEnd; i++)
-         {
-           $(tr[i]).show();
-         }
-       });
-    });
+      else if (!$('input[name=gender]:checked').val() ) {
+        alert(":Select your gender");
+        GetTable();
+        return false;
+      }
+      else if (!$('input[name=inter]:checked').val()){
+        alert(":Select your interests");
+        GetTable();
+        return false;
+      }
+      return true;
+    }
 
+
+    //add new row to table
+    function addrow() {
+      $('#table').prepend("<tbody><tr><td>" + user.Id + "</td><td>"
+                                    + user.FirstName + "</td><td>"
+                                    + user.LastName +"</td><td>"
+                                    + user.Age +"</td><td>"
+                                    + user.Gender +"</td><td>"
+                                    + user.Interests +"</td><td>"
+                                    + user.Country + "</td></tr></tbody>");
+    }
 
 
        //save function
     $('#btnsave').on('click', function() {
-            user ={
-            Id : id,
-            FirstName : $("#firstname").val(),
-            LastName : $('#lastname').val(),
-            Age : $("#age").val(),
-            Gender : $(':radio[name=gender]:checked').val(),
-            Interests : $(':checkbox[name=inter]:checked').map(function() {
-              return $(this).val();
-            }).get(),
-            Country :  $("#country").val()
-          };
-
-          //validation of form
-          if($("#firstname").val() == '' | $('#lastname').val() == '' | $("#age").val() == '' | $("#age").val() >111){
-            alert(":Incorrect Name or Age");
-          }
-          else if (!$('input[name=gender]:checked').val() ) {
-            alert(":Select your gender");
-          }
-          else if (!$('input[name=inter]:checked').val()){
-            alert(":Select your interests");}
-          else {
-
-          // Add data to array and table
+      setUser();
+      if (valodateForm()=== true){
           tableUsers.unshift(user);
           localStorage.setItem('users', JSON.stringify(tableUsers));
+          addrow();
           $('#frm')[0].reset();
-          $('#table').prepend("<tbody><tr><td>" + user.Id + "</td><td>"
-                                        + user.FirstName + "</td><td>"
-                                        + user.LastName +"</td><td>"
-                                        + user.Age +"</td><td>"
-                                        + user.Gender +"</td><td>"
-                                        + user.Interests +"</td><td>"
-                                        + user.Country + "</td></tr></tbody>");
+          id++;
+          localStorage.setItem("id", id);
+          Pag();
+          return false;
+        }
+    });
 
-            id++;
-            localStorage.setItem("id", id);
-            Pag();
-            return false;
-          }
-        });
-
+      //save sorted array
+      function saveSort() {
+        for ( var i = 1; i < table.rows.length; i++ ) {
+            sortedTab.push({
+                Id: table.rows[i].cells[0].innerHTML,
+                FirstName: table.rows[i].cells[1].innerHTML,
+                LastName: table.rows[i].cells[2].innerHTML,
+                Age: table.rows[i].cells[3].innerHTML,
+                Gender: table.rows[i].cells[4].innerHTML,
+                Interests: table.rows[i].cells[5].innerHTML,
+                Country: table.rows[i].cells[6].innerHTML
+              });
+            }
+      }
 
         // sortable function
         $('#table').sortable({
@@ -120,22 +120,10 @@
             placeholder: '',
             revert: true,
             update: function(){
-              sortedTab = [];
-              for ( var i = 1; i < table.rows.length; i++ ) {
-                  sortedTab.push({
-                      Id: table.rows[i].cells[0].innerHTML,
-                      FirstName: table.rows[i].cells[1].innerHTML,
-                      LastName: table.rows[i].cells[2].innerHTML,
-                      Age: table.rows[i].cells[3].innerHTML,
-                      Gender: table.rows[i].cells[4].innerHTML,
-                      Interests: table.rows[i].cells[5].innerHTML,
-                      Country: table.rows[i].cells[6].innerHTML
-                    });
-                  }
+              saveSort();
               localStorage.setItem("users", JSON.stringify(sortedTab));
              }
           });
-
 
 
           //Selecting row
@@ -172,8 +160,7 @@
           });
 
 
-
-        //deleting functions
+        //deleting function
       $('#btndelete').click(function() {
         table.rows[rIndex].remove();
         tableUsers.splice(rIndex-1, 1);
@@ -183,30 +170,70 @@
         });
 
 
+        //editing of selected array item
+        function editArrayItem() {
+          tableUsers[rIndex-1]={
+              Id: table.rows[rIndex].cells[0].innerHTML,
+              FirstName: table.rows[rIndex].cells[1].innerHTML,
+              LastName: table.rows[rIndex].cells[2].innerHTML,
+              Age: table.rows[rIndex].cells[3].innerHTML,
+              Gender: table.rows[rIndex].cells[4].innerHTML,
+              Interests: table.rows[rIndex].cells[5].innerHTML,
+              Country: table.rows[rIndex].cells[6].innerHTML
+          };
+        }
+
+        //editing of selected table row
+        function editSelectedRow() {
+          table.rows[rIndex].cells[1].innerHTML = $("#firstname").val();
+          table.rows[rIndex].cells[2].innerHTML = $("#lastname").val();
+          table.rows[rIndex].cells[3].innerHTML = $("#age").val();
+          table.rows[rIndex].cells[4].innerHTML = $(':radio[name=gender]:checked').val();
+          table.rows[rIndex].cells[5].innerHTML = $(':checkbox[name=inter]:checked').map(function() {
+              return $(this).val();
+            }).get();
+          table.rows[rIndex].cells[6].innerHTML = $("#country").val();
+        }
 
 
         // edit function
       $("#btnedit").on("click", function() {
-        table.rows[rIndex].cells[1].innerHTML = $("#firstname").val();
-        table.rows[rIndex].cells[2].innerHTML = $("#lastname").val();
-        table.rows[rIndex].cells[3].innerHTML = $("#age").val();
-        table.rows[rIndex].cells[4].innerHTML = $(':radio[name=gender]:checked').val();
-        table.rows[rIndex].cells[5].innerHTML = $(':checkbox[name=inter]:checked').map(function() {
-            return $(this).val();
-          }).get();
-        table.rows[rIndex].cells[6].innerHTML = $("#country").val();
-
-        tableUsers[rIndex-1]={
-            Id: table.rows[rIndex].cells[0].innerHTML,
-            FirstName: table.rows[rIndex].cells[1].innerHTML,
-            LastName: table.rows[rIndex].cells[2].innerHTML,
-            Age: table.rows[rIndex].cells[3].innerHTML,
-            Gender: table.rows[rIndex].cells[4].innerHTML,
-            Interests: table.rows[rIndex].cells[5].innerHTML,
-            Country: table.rows[rIndex].cells[6].innerHTML
-        };
+        editSelectedRow();
+        editArrayItem();
         localStorage.setItem('users', JSON.stringify(tableUsers));
         $('#frm')[0].reset();
         Pag();
+      });
+
+
+      //Pagination function
+
+      $(function Pag() {
+        var totalRows = $('#table').find('tbody tr:has(td)').length;
+        var recordPerPage = 10;
+        var totalPages = Math.ceil(totalRows / recordPerPage);
+        var $pages = $('<div id="pages" class="pagin" ></div>');
+        for (i = 0; i < totalPages; i++) {
+            $('<button type="button" class="pagination">&nbsp;' + (i + 1) + '</button>').appendTo($pages);
+        }
+         $pages.appendTo('#pagin');
+         $('.pagination').hover(
+           function() { $(this).addClass('focus'); },
+           function() { $(this).removeClass('focus'); }
+         );
+         $('table').find('tbody tr:has(td)').hide();
+         var tr = $('table tbody tr:has(td)');
+         for (var i = 0; i <= recordPerPage - 1; i++) {
+           $(tr[i]).show();
+         }
+         $('button').on('click', function(event) {
+           $('#table').find('tbody tr:has(td)').hide();
+           var nBegin = ($(this).text() - 1) * recordPerPage;
+           var nEnd = $(this).text() * recordPerPage - 1;
+           for (var i = nBegin; i <= nEnd; i++)
+           {
+             $(tr[i]).show();
+           }
+         });
       });
 });
