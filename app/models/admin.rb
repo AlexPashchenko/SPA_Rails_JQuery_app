aaf1b2761765ee2425b45026fea8f2547953de79
AlexPashchenko <sashka.gag@gmail.com>
2017-08-22 17:47:04 +0300

    Create admin controller and add devise to admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,7 @@
 class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
  validates_email_format_of :email, :message => 'is not looking good'
  validates :password, confirmation: true
